395a91bc1540c07b8e38a45610e4ad7d68a7b5bd
kelvin17 <qyq1226@gmail.com>
2024-05-31 00:09:58 +0800

    请求参数注解每大

--- src/main/java/com/tencent/wxcloudrun/controller/CoachController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CoachController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,7 +31,7 @@ public class CoachController {
      * @return
      */
     @PostMapping(value = "/api/coach/create_activity")
-    public ApiResponse createActivity(ActivityRequest request) {
+    public ApiResponse createActivity(@RequestBody ActivityRequest request) {
         //获取登陆态
         logger.info("the request {}", request);
         String coachName = request.getCoachName();
