18d3ca6401781b6c29d65056e0fba90d51f15131
kelvin17 <qyq1226@gmail.com>
2024-05-30 23:21:16 +0800

    BUG FREE

--- src/main/java/com/tencent/wxcloudrun/controller/CoachController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CoachController.java
@@ -38,6 +38,8 @@ public class CoachController {
         activity.setCoachName(coachName);
         activity.setDescription(request.getDescription());
         activity.setDate(request.getDate());
+        activity.setLimitCountLower(request.getLimitLower());
+        activity.setLimitCountHigher(request.getLimitHigher());
         ApiResponse response;
         try {
             coachService.createActivity(activity);
